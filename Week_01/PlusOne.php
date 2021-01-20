1b97128a32870de828d03e06348a605d80ad4fa3
dongjie1 <dongjie1@staff.weibo.com>
2021-01-20 23:26:47 +0800

    加1

---
+++ Week_01/PlusOne.php
@@ -0,0 +1,26 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $digits
+     * @return Integer[]
+     */
+    function plusOne($digits) {
+        if(empty($digits)) return [];
+
+        $len = count($digits)-1;
+        for($i=$len; $i>=0; $i--){
+            $digits[$i]++;
+            $digits[$i] %= 10;
+            if($digits[$i] != 0){
+                return $digits;
+            }elseif($i == 0){//遍历完了digits[$i]=0说明还有进位,在数组前面插入1
+//                array_unshift($digits,1);
+                $digits[0] = 1;
+                $digits[] = 0;
+            }
+        }
+
+        return $digits;
+    }
+}
