e84d693e0060035741d6f9fd4fe31e58725d86e7
dongjie1 <dongjie1@staff.weibo.com>
2021-01-21 23:10:30 +0800

    字母异位词分组

---
+++ Week_01/ValidGroupAnagram.php
@@ -0,0 +1,43 @@
+<?php
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String[][]
+     * 用排序后的单词为key，值为排序后相同的词数组
+     */
+    function groupAnagrams($strs) {
+        $res = [];
+        foreach($strs as $str){
+            $s_arr = str_split($str);
+            sort($s_arr);
+            $_s = implode('',$s_arr);
+            $tmp[$_s][] = $str;
+        }
+
+        return $res;
+    }
+
+    /**
+     * @param $strs
+     * @return array
+     * 字符计数方法
+     * 这种方法key太长
+     */
+    function groupAnagrams1($strs){
+        $res = [];
+        foreach ($strs as $str){
+            $tmp = array();
+            for($i=0; $i<strlen($str); $i++){
+                $tmp[ord($str[$i])]++;
+            }
+            $key = '';
+            for($i=ord('a'); $i<=ord('z'); $i++){
+                $key .= '#'.$tmp[$i]?:0;
+            }
+            var_dump($key);
+            $res[$key][] = $str;
+        }
+        return $res;
+    }
+}
