abea887859f191f208bf9759fc36824940e330ec
dongjie1 <dongjie1@staff.weibo.com>
2021-01-27 13:24:58 +0800

    n叉树后序遍历

---
+++ Week_02/NTreePostOrder.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * N叉树后序遍历
+ */
+class Solution {
+    /**
+     * 递归
+     * @param Node $root
+     * @return integer[]
+     */
+    private $data = [];
+    function postorder2($root) {
+        if(!$root) return [];
+
+        foreach($root->children as $child){
+            $this->postorder($child);
+        }
+        $this->data[] = $root->val;
+        return $this->data;
+    }
+
+    /**
+     * 迭代
+     * @param $root
+     * @return array
+     *
+     */
+    function postorder($root){
+        if(!$root) return $this->data;
+
+        $result = [];
+        $stack = [$root];
+
+        while (!empty($stack)){
+            $node = array_pop($stack);
+            $result[] = $node->val;
+
+            foreach ($node->children as $child){
+                if (!empty($child)){
+                    $stack[] = $child;
+                }
+            }
+        }
+
+        $result = array_reverse($result);
+        return $result;
+    }
+}
