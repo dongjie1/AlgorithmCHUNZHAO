b3bfad8536ffe17ad7da06be94e4777aa2e837ad
dongjie1 <dongjie1@staff.weibo.com>
2021-01-28 12:04:42 +0800

    二叉树的节点

---
+++ Week_02/TreeNode.php
@@ -0,0 +1,11 @@
+<?php
+class TreeNode {
+    public $val = null;
+    public $left = null;
+    public $right = null;
+    function __construct($val = 0, $left = null, $right = null) {
+            $this->val = $val;
+            $this->left = $left;
+            $this->right = $right;
+        }
+}
