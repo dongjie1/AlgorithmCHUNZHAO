144aaf98997e48dfc9a4cf0ab43107ed21f36fa5
dongjie1 <dongjie1@staff.weibo.com>
2021-02-02 21:28:43 +0800

    电话号码的字母组合

--- Week_03/LetterCombinations.php
+++ Week_03/LetterCombinations.php
@@ -6,14 +6,14 @@
 
 class Solution {
     private $letter_map = array(
-        2 => 'abc',
-        3 => 'def',
-        4 => 'ghi',
-        5 => 'jkl',
-        6 => 'mno',
-        7 => 'pqrs',
-        8 => 'tuv',
-        9 => 'wxyz'
+        2 => ['a','b','c'],
+        3 => ['d','e','f'],
+        4 => ['g','h','i'],
+        5 => ['j','k','l'],
+        6 => ['m','n','o'],
+        7 => ['p','q','r','s'],
+        8 => ['t','u','v'],
+        9 => ['w','x','y','z']
     );
     /**
      * @param String $digits
@@ -36,7 +36,6 @@ class Solution {
 
         //process
         $letters = $this->letter_map[$digits{$i}];
-        $letters = str_split($letters);
 
         foreach ($letters as $char) {
             //drill down
