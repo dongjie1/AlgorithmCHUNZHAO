f8776457eb8194e3cdc19e9f5433d09e1503154c
dongjie1 <dongjie1@staff.weibo.com>
2021-02-05 16:40:22 +0800

    查找一个半无序数组中无序的地方

---
+++ Week_03/SearchNoSortIndex.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 一个半无序数组，查找无序的地方
+ * @param array $nums 半无序数组
+ * @return int[] 返回无序地方的索引
+ */
+
+function search($nums){
+    $left = 0;
+    $right = count($nums)-1;
+
+    while ($left <= $right) {
+        $mid = $left + intval(($right-$left)/2);
+        if ($nums[$mid] < $nums[$mid-1] && $nums[$mid]<$nums[$mid+1]){
+            return [$mid-1,$mid];
+        }elseif($nums[$mid] > $nums[$right]){
+            $left = $mid + 1;
+        }else{
+            $right = $mid - 1;
+        }
+    }
+    return [-1,-1];
+}
+
+$nums = [4, 5, 6, 7,9, 0, 1, 2,3];
+$index = search($nums);
+var_dump($index);
