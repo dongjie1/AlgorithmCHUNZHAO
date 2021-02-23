4b270cde68b4c2e445e26125e99825571aea86a6
dongjie1 <dongjie1@staff.weibo.com>
2021-02-23 23:36:21 +0800

    并查集实现

---
+++ Week_05/UnionFind.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 并查集实现
+ */
+class UnionFind{
+    private $parent = [];
+    private $count = 0;
+
+    public function add($x) {
+        if (!isset($this->parent[$x])) {
+            $this->parent[$x] = null;
+            $this->count++;
+        }
+    }
+
+    public function find($x) {
+        $root = $x;
+        while ($this->parent[$root]) {
+            $root = $this->parent[$root];
+        }
+
+        //压缩路径
+        while ($root != $x) {
+            $ap = $this->parent[$x];
+            $this->parent[$x] = $root;
+            $x = $ap;
+        }
+        return $root;
+    }
+
+    public function merge($x, $y) {
+        $rootX = $this->find($x);
+        $rootY = $this->find($y);
+
+        if ($rootX != $rootY) {
+            $this->parent[$rootX] = $rootY;
+            $this->count--;
+        }
+    }
+
+    public function getNums() {
+        return $this->count;
+    }
+
+
+}
