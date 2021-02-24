16b7f25425376410a4b04bc719c5fdb181d5c7fe
dongjie1 <dongjie1@staff.weibo.com>
2021-02-24 22:21:09 +0800

    岛屿数量-并查集实现

--- Week_05/NumIsLands.php
+++ Week_05/NumIsLands.php
@@ -12,21 +12,33 @@ class Solution{
      * 使用并查集
      */
     function numIsLands($grid){
-        $len = count($grid);
+        $rows = count($grid);
+        $cols = count($grid[0]);
 
-        if ($len == 1) {
-            if ($grid[0][0] == '0') return 0;
-            if ($grid[0][0] == '1') return 1;
-        }
+        if ($rows == 0) return 0;
 
-        $uf = new UnionFind($len);
-        for ($i=0; $i<$len; $i++) {
-            for ($j=$i+1; $j<$len; $j++) {
-                if ($grid[$i][$j] == 1) {
-                    $uf->merge($i,$j);
+        $spaces = 0;//空地的数量
+
+        $uf = new UnionFind($rows * $cols);//初始化变为1维数组数量
+        for ($i=0; $i<$rows; $i++) {
+            for ($j=0; $j<$cols; $j++) {
+                if ($grid[$i][$j] == 0){
+                    $spaces++;
+                }elseif ($grid[$i][$j] == 1) {//如果此位置的右边和下边为1则合并
+                    $idx = $i*$cols+$j;//当前位置变成1维数组的索引
+                    $down = ($i+1)*$cols+$j;//下边位置变成1维数组的索引
+                    $right = $i*$cols+($j+1);//右面位置变成1维数组的索引
+
+                    if ($i+1 < $rows && $grid[$i+1][$j] == 1){//下边为1合并
+                        $uf->merge($idx,$down);
+                    }
+                    if ($j+1 < $cols && $grid[$i][$j+1] == 1){//右边为1合并
+                        $uf->merge($idx,$right);
+                    }
+
                 }
             }
         }
-        return $uf->getNums();
+        return $uf->getNums()-$spaces;
     }
 }
