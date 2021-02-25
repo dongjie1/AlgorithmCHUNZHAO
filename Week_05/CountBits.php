5536da728ac9bdf67ffd376f24837bc6ee3bad07
dongjie1 <dongjie1@staff.weibo.com>
2021-02-25 12:50:48 +0800

    比特位计数

--- Week_05/CountBits.php
+++ Week_05/CountBits.php
@@ -11,6 +11,8 @@ class Solution {
      * 判断奇偶
      * 奇数1的个数=前一个数的1的个数+1
      * 偶数1的个数=i/2的数的1的个数
+     * 时间复杂度: O(n)
+     * 空间复杂度: O(n)
      */
     function countBits($num) {
         $result[0] = 0;
@@ -30,6 +32,8 @@ class Solution {
      * @param $num
      * @return array
      * 使用 n&(n-1)最低位置0计算每个数的1的个数
+     * 时间复杂度: O(n)
+     * 空间复杂度: O(n)
      */
     function countBits2($num) {
         $result[0] = 0;
@@ -53,5 +57,21 @@ class Solution {
             $num &= $num -1; //每次最低位置0,直到num=0
         }
         return $n;
+    }
+
+    /**
+     * @param $num
+     * @return array
+     * 时间复杂度: O(n)
+     * 空间复杂度: O(n)
+     */
+    function countBits3($num) {
+        $result[0] = 0;
+        if($num == 0) return $result;
+
+        for ($i=1; $i<=$num; $i++){
+            $result[$i] = $result[$i&($i-1)] + 1;//i的1的个数=i&(i-1)的1的个数+1
+        }
+        return $result;
     }
 }
